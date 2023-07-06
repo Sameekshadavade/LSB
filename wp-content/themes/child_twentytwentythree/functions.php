aeb3f0c1e443945de7f48edd202a5c3e5cecffd3
Sameekshadavade <sameeksha.d@chapter247.com>
2023-07-06 16:26:41 +0530

    style.css added

--- wp-content/themes/child_twentytwentythree/functions.php
+++ wp-content/themes/child_twentytwentythree/functions.php
@@ -1,17 +1,5 @@
 <?php
-/*
-Theme Name:   TwentyTwentythree
-Theme URI:    
-Description:  A Twenty Twenty-One child theme 
-Author:       WPBeginner
-Author URI:   
-Template:     twentytwentythree
-Version:      1.0.0
-Text Domain:  twentytwentythreechild
-*/
-
 /* enqueue scripts and style from parent theme */
-    
 function twentytwentythree_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_uri(),array( 'twenty-twenty-three-style' ), wp_get_theme()->get('Version') );
 }
