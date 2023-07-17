08c42d90c773bb40a30e81ebbf557876cbdb201c
Sameekshadavade <sameeksha.d@chapter247.com>
2023-07-17 10:43:25 +0530

    header.php added

---
+++ wp-content/themes/childtwentytwentythree/header.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>LSBAuctions</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="" rel="icon">
+
+    <!-- Google Fonts -->
+    <!-- <link rel="stylesheet" href="fonts/font-style.css">
+    <link rel="stylesheet" href="fonts/font-icon"> -->
+
+    <!-- CSS Files -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <!-- ======= Start-Header-Area ======= -->
+    <header id="header" class="header d-flex align-items-center">
+        <div id="site-nav-bar" class="site-nav-bar">
+            <div class="upper-bar">
+                <div class="site-brand">
+                    <a href="#">
+                        <img src="img/lsb_inline_light-tan.svg" alt="Lock, Stock &amp; Barrel Investments">
+                    </a>
+                </div>
+                <div>
+                    <ul class="utility-section">
+                        <li>
+                            <h5 class="company-phone-number"><small>Phone:</small>805.285.0715</h5>
+                        </li>
+                        <li class="search-bar">
+                            <form role="search" method="get" class="searchform"
+                                action="#">
+                                <fieldset>
+                                    <legend class="visibility-hidden">Search</legend>
+                                    <input type="text" class="s form-control search-label" name="s"
+                                        placeholder="Search…" value="" aria-label="Search">
+                                </fieldset>
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="lower-bar">
+                <div class="container-fluid">
+                    <nav id="navbar" class="navbar w-100">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li class="dropdown"><a href="#"><span>Company</span> <i
+                                        class="bi bi-chevron-down dropdown-indicator"></i></a>
+                                <ul>
+                                    <li><a href="#">About Us</a></li>
+                                    <li><a href="#">Our Staff</a></li>
+                                    <li><a href="#">Spencer Hoglund</a></li>
+                                    <li><a href="#">Our Collection</a></li>
+                                    <li><a href="#">Articles</a></li>
+                                    <li><a href="#">Careers</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown"><a href="#"><span>Buy & Sell</span> <i
+                                        class="bi bi-chevron-down dropdown-indicator"></i></a>
+                                <ul>
+                                    <li><a href="#">Our Process</a></li>
+                                    <li><a href="#">Acquisition</a></li>
+                                    <li><a href="#">Auctions</a></li>
+                                    <li><a href="#">Sell</a></li>
+                                    <li><a href="#">FAQs</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Sold Items</a></li>
+                            <li><a href="#">Get Started</a></li>
+                            <li><a href="#">Make Payment</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </nav><!-- .navbar -->
+                    <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+                    <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ======= End-Header-Area ======= -->
+
+    <!-- ======= Start-Main-Area ======= -->
+    <div class="main-wrapper-area"></div>
+    <!-- ======= End-Main-Area ======= -->
+
+    <!-- ======= Start-Footer-Area ======= -->
+    <footer id="footer" class="footer">
+        <div class="container">
+            <div class="row gy-3">
+                <div class="col-lg-3 col-md-6 d-flex">
+                    <i class="bi bi-geo-alt icon"></i>
+                    <div>
+                        <h4>Address</h4>
+                        <p>
+                            A108 Adam Street <br>
+                            New York, NY 535022 - US<br>
+                        </p>
+                    </div>
+
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-links d-flex">
+                    <i class="bi bi-telephone icon"></i>
+                    <div>
+                        <h4>Reservations</h4>
+                        <p>
+                            <strong>Phone:</strong> +1 5589 55488 55<br>
+                            <strong>Email:</strong> info@example.com<br>
+                        </p>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-links d-flex">
+                    <i class="bi bi-clock icon"></i>
+                    <div>
+                        <h4>Opening Hours</h4>
+                        <p>
+                            <strong>Mon-Sat: 11AM</strong> - 23PM<br>
+                            Sunday: Closed
+                        </p>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Follow Us</h4>
+                    <div class="social-links d-flex">
+                        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+    <!-- ======= End-Footer-Area ======= -->
+
+    <!-- JS Files -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/custom.js"></script>
+
+</body>
+
+</html>
