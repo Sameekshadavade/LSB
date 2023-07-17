a7c35ac206d3f5090c19efee731862d9c1574adf
Sameekshadavade <sameeksha.d@chapter247.com>
2023-07-17 11:58:20 +0530

    style.css added

--- wp-content/themes/childtwentytwentythree/functions.php
+++ wp-content/themes/childtwentytwentythree/functions.php
@@ -3,17 +3,11 @@
 
 	 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles' );
 	 function child_enqueue_styles() {
- 		  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
- 		  } 
+ 		  wp_enqueue_style( 'parent-style_theme',  get_stylesheet_directory_uri() . '/style.css' ); 
+           wp_enqueue_style( 'Bundle_js', get_stylesheet_directory_uri(). '/Javascript/bootstrap.bundle.min.js', array(), null, 'all');
+ 		  
+           wp_enqueue_style( 'Custom_js', get_stylesheet_directory_uri(). '/Javascript/custom.js', array(), null, 'all');
+        } 
  
-echo  get_stylesheet_directory_uri()();
-// function add_theme_scripts() {
 
-//     wp_enqueue_style( 'Bundle_js', get_stylesheet_directory_uri(). '/css/persian-datepicker.css', array(), null, 'all');
-  
-//     wp_enqueue_script( 'jsdate', get_template_directory_uri() . '/js/persian-date.js', array ( 'jquery' ), null, true);
-//      wp_enqueue_script( 'jsdatepicker', get_template_directory_uri() . '/js/persian-datepicker.js', array ( 'jquery' ), null, true);
-  
-//   }
-//   add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
 ?>
--- wp-content/themes/childtwentytwentythree/header.php
+++ wp-content/themes/childtwentytwentythree/header.php
@@ -7,21 +7,8 @@
     <title>LSBAuctions</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
-
-    <!-- Favicons -->
     <link href="" rel="icon">
-
-    <!-- Google Fonts -->
-    <!-- <link rel="stylesheet" href="fonts/font-style.css">
-    <link rel="stylesheet" href="fonts/font-icon"> -->
-
-    <!-- CSS Files -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-
 </head>
-
 <body>
 
     <!-- ======= Start-Header-Area ======= -->
@@ -89,68 +76,8 @@
             </div>
         </div>
     </header>
-    <!-- ======= End-Header-Area ======= -->
+   
 
-    <!-- ======= Start-Main-Area ======= -->
-    <div class="main-wrapper-area"></div>
-    <!-- ======= End-Main-Area ======= -->
-
-    <!-- ======= Start-Footer-Area ======= -->
-    <footer id="footer" class="footer">
-        <div class="container">
-            <div class="row gy-3">
-                <div class="col-lg-3 col-md-6 d-flex">
-                    <i class="bi bi-geo-alt icon"></i>
-                    <div>
-                        <h4>Address</h4>
-                        <p>
-                            A108 Adam Street <br>
-                            New York, NY 535022 - US<br>
-                        </p>
-                    </div>
-
-                </div>
-
-                <div class="col-lg-3 col-md-6 footer-links d-flex">
-                    <i class="bi bi-telephone icon"></i>
-                    <div>
-                        <h4>Reservations</h4>
-                        <p>
-                            <strong>Phone:</strong> +1 5589 55488 55<br>
-                            <strong>Email:</strong> info@example.com<br>
-                        </p>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 footer-links d-flex">
-                    <i class="bi bi-clock icon"></i>
-                    <div>
-                        <h4>Opening Hours</h4>
-                        <p>
-                            <strong>Mon-Sat: 11AM</strong> - 23PM<br>
-                            Sunday: Closed
-                        </p>
-                    </div>
-                </div>
-
-                <div class="col-lg-3 col-md-6 footer-links">
-                    <h4>Follow Us</h4>
-                    <div class="social-links d-flex">
-                        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-                        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </footer>
-    <!-- ======= End-Footer-Area ======= -->
-
-    <!-- JS Files -->
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/custom.js"></script>
 
 </body>
 
