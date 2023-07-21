6dd75b6f32e244ab5b372e0275cc34c02478bf8f
AsheeshChapter247 <aaseesh.g@chapter247.com>
2023-07-21 17:26:19 +0530

    Worked design on about page

--- wp-content/themes/childtwentytwentythree/aboutus.php
+++ wp-content/themes/childtwentytwentythree/aboutus.php
@@ -4,9 +4,133 @@
 <!-- ======= Start-Main-Area ======= -->
 <div class="main-wrapper-area">
 
-<!-- ======= Start-Banner-Area ======= -->
+    <!-- ======= Start-Banner-Area ======= -->
+    <section class="banner-section about">
+        <div class="banner-content">
+            <img class="lsb-brand" src="/LSB/wp-content/themes/childtwentytwentythree/assets/images/lsb_letters-white.svg" alt="LSB">
+            <h2>We take your satisfaction <em>very</em> seriously</h2>
+        </div>
+    </section>
+    <!-- ======= End-Banner-Area ======= -->
 
-<!-- ======= End-Banner-Area ======= -->
+    <div class="container page-content">
+        <section class="common-header mb-5">
+            <header class="page-header">
+                <h1 class="page-title">About Us</h1>
+            </header>
+        </section>
+        <section class="history-section pt-md-5">
+            <div class="row">
+                <div class="col-md-8">
+                    <h2>Our History</h2>
+                    <p>
+                        Lock, Stock and Barrel specializes in the acquisition and disposition of firearms, firearm paraphernalia,
+                        reloading equipment and military/war memorabilia. We acquire items of any vintage, and any condition, from
+                        individuals and estates nationwide for any liquidation purpose. We will purchase or consign single items or
+                        entire collections – with a proven track record of achieving better than market prices for our sellers.
+                    </p>
+                    <p>
+                        Lock, Stock &amp; Barrel was started by Spencer Hoglund, a noted firearms collector, researcher and a World
+                        Champion
+                        Cowboy Shooter. Spencer realized gun owners were in need of an honest and reliable destination for selling
+                        their
+                        guns and related items. Lock, Stock &amp; Barrel has grown to be the largest auction house of its kind on the West
+                        Coast.
+                    </p>
+                </div>
+                <div class="col-md-4">
+                    <div class="banner">
+                        <img src="/LSB/wp-content/themes/childtwentytwentythree/assets/images/100-percent-feedback_square.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="row lsb-difference mt-5 mb-5">
+            <div class="col">
+                <h2 class="section-heading"><span>The Lock, Stock &amp; Barrel Difference</span></h2>
+                <ul class="two-col circle-checkmark">
+                    <li>
+                        <h3>Flat Commission</h3>
+                        <p>There are no hidden fees, ever. You will see every detail of the transaction from beginning to end. With
+                            <em>No Buyer’s Premium</em>.
+                        </p>
+                    </li>
+                    <li>
+                        <h3>We Sell Without A Buyer’s Premium</h3>
+                        <p>We strive to deliver the most money possible to our sellers and deliver quality merchandise without added
+                            fees to our buyers.</p>
+                    </li>
+                    <li>
+                        <h3>Guaranteed Sales Dates &amp; Payment Dates</h3>
+                        <p>You will know when your items are going to auction, and when you can expect your proceeds.</p>
+                    </li>
+                    <li>
+                        <h3>Timely Auctions</h3>
+                        <p>&ZeroWidthSpace;Over 3 million page visitors will have the opportunity to see your merchandise. We ensure that we
+                            maximize this exposure with 14-day auctions.</p>
+                    </li>
+                    <li>
+                        <h3>Photography &amp; Descriptions</h3>
+                        <p>Your items will be marketed with world class image quality, quantity, and highly detailed descriptions of
+                            each aspect of your items.</p>
+                    </li>
+                    <li>
+                        <h3>Online Auction Presence</h3>
+                        <p>Buyers can bid anytime/anywhere, and sellers can monitor their items in real time.</p>
+                    </li>
+                    <li>
+                        <h3>Our Descriptions Are Very Detailed</h3>
+                        <p>We highlight every aspect of our items – good or bad, we encourage you to compare our descriptions with
+                            any
+                            of our competitors to see the Lock Stock &amp; Barrel difference.</p>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <section class="row user-feedback">
+            <div class="col">
+                <h2 class="section-heading"><span>Buyer Feedback</span></h2>
+                <p>Over the years, we’ve listed and sold tens of thousands of guns and related merchandise through online
+                    auction.
+                    We take your satisfaction <em>VERY SERIOUSLY</em> at Lock Stock &amp; Barrel, and you can see this in our
+                    unmatched
+                    feedback on all sites.</p>
+                <p class="note-block">
+                    &ZeroWidthSpace;Please note: The links below will open a new window to the reviews section on each respective site.
+                </p>
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 feedback-link">
+                        <a href="#" target="_blank">
+                            <img src="/LSB/wp-content/themes/childtwentytwentythree/assets/images/yelp_brand.svg" alt="Yelp">
+                            <span>Read Feedback</span>
+                            <span class="screen-reader-text">Opens a new window</span>
+                        </a>
+                    </div>
+                    <div class="col-sm-6 col-md-3 feedback-link">
+                        <a href="#" target="_blank">
+                            <img src="/LSB/wp-content/themes/childtwentytwentythree/assets/images/gun-broker_brand.svg" alt="">
+                            <span>Read Feedback</span>
+                            <span class="screen-reader-text">Opens a new window</span>
+                        </a>
+                    </div>
+                    <div class="col-sm-6 col-md-3 feedback-link">
+                        <a href="#" target="_blank">
+                            <img src="/LSB/wp-content/themes/childtwentytwentythree/assets/images/ebay_brand.svg" alt="">
+                            <span>Read Feedback</span>
+                            <span class="screen-reader-text">Opens a new window</span>
+                        </a>
+                    </div>
+                    <div class="col-sm-6 col-md-3 feedback-link">
+                        <a href="#" target="_blank">
+                            <img src="/LSB/wp-content/themes/childtwentytwentythree/assets/images/gun-auction_brand.png" alt="">
+                            <span>Read Feedback</span>
+                            <span class="screen-reader-text">Opens a new window</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
 
 </div>
 <!-- ======= End-Main-Area ======= -->
