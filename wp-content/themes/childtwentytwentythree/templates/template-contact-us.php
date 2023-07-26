a7d3a2bddf4e6756db8fcdfeae8d7dedb655bbae
Yogendra Jain <yogendra.j@chapter247.com>
2023-07-26 14:20:58 +0530

    design contact terms pages

---
+++ wp-content/themes/childtwentytwentythree/templates/template-contact-us.php
@@ -0,0 +1,84 @@
+<?php /* Template Name: contact us */ ?>
+
+<?php get_header(); ?>
+
+<!-- ======= Start-Main-Area ======= -->
+<div class="main-wrapper-area">
+
+    <!-- ======= Start-Banner-Area ======= -->
+    <section class="banner-section contact">
+        <div class="banner-content">
+            <h2>Contacting Lock,<br>Stock & Barrel</h2>
+        </div>
+    </section>
+    <!-- ======= End-Banner-Area ======= -->
+
+    <div class="container page-content">
+        <section class="common-header mb-5">
+            <header class="page-header">
+                <h1 class="page-title">CONTACT & DIRECTIONS</h1>
+            </header>
+        </section>
+
+
+
+        <section class="row">
+            <div class="col-lg-8">
+                <h2>Contacting Us Is Easy</h2>
+                <p>
+                    We are at the office 6-days a week and work diligently to respond to you as promptly as possible.
+                    <strong>Please call us to make an appointment, no walk-ins</strong>.
+                </p>
+                <p>
+                    You can email us questions, concerns, or join our weekly email list using the form below.
+                    Alternatively, pick up the phone and call our friendly staff anytime (805) 285-0715.
+                </p>
+
+                <div>
+
+
+                </div>
+
+            </div>
+            <div class="col-lg-4">
+                <ul class="company-info">
+                    <li>
+                        <h6>Lock, Stock & Barrel Investments</h6>
+                        <p>94 W. Cochran St., Suite B<br>Simi Valley, CA 93065</p>
+                    </li>
+                    <li>
+                        <h6>Phone</h6>
+                        <p>(805) 285-0715</p>
+                    </li>
+                    <li>
+                        <h6>Fax</h6>
+                        <p>(805) 285-0729</p>
+                    </li>
+                    <li>
+                        <h6>Hours</h6>
+                        <p>
+                            Monday &ndash; Friday 8AM to 5PM<br>
+                            Saturdays By Appointment<br>
+                            Closed Sundays<br>
+                        </p>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <section>
+            <h2 class="section-heading"><span>Directions</span></h2>
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15951.153226226705!2d-118.799773!3d34.282055!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80e82ed289a4eb19%3A0x9d1a9ffda088bc1f!2sLock%20Stock%20%26%20Barrel%20Investments%20(Online%20store%20only)!5e1!3m2!1sen!2sus!4v1690356649402!5m2!1sen!2sus"
+                width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </section>
+
+
+
+
+    </div>
+
+</div>
+<!-- ======= End-Main-Area ======= -->
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/childtwentytwentythree/templates/template-payments.php
@@ -0,0 +1,44 @@
+<?php /* Template Name: Payments */ ?>
+
+<?php get_header(); ?>
+
+<!-- ======= Start-Main-Area ======= -->
+<div class="main-wrapper-area">
+
+
+    <div class="container page-content">
+        <section class="common-header mb-5">
+            <header class="page-header">
+                <h1 class="page-title">PAY FOR YOUR AUCTION</h1>
+            </header>
+        </section>
+
+
+
+        <section class="row ">
+            <div class="col-sm-12 mt-5">
+                <h2>We appreciate your business</h2>
+                <p>
+                    Please use the form below to make a safe and secure payment to Lock, Stock &amp; Barrel.
+                    If you should have any issues with your transaction, you can either <a
+                        class='text-decoration-underline' href="contact-us">contact us
+                        here</a>
+                    or
+                    give us a call at <strong>805-285-0715</strong> and our friendly staff will be happy to
+                    help.
+                </p>
+            </div>
+            <div class="col-12 mt-4">
+                <h2 class="payment-title">Payment Form</h2>
+            </div>
+        </section>
+
+
+
+
+    </div>
+
+</div>
+<!-- ======= End-Main-Area ======= -->
+
+<?php get_footer(); ?>
