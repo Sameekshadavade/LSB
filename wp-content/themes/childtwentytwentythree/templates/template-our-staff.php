7b79d8a74b67c9ed6e8e6bbade0cd07563541ab3
dharmendra-c247 <dharmendra.v@chapter247.com>
2023-07-26 14:23:29 +0530

    our staff page dynamic

--- wp-content/themes/childtwentytwentythree/templates/template-our-staff.php
+++ wp-content/themes/childtwentytwentythree/templates/template-our-staff.php
@@ -2,116 +2,60 @@
 
 <?php get_header(); ?>
 <!-- ======= Start-Main-Area ======= -->
+<?php  
+
+$feat_image = wp_get_attachment_url(get_post_thumbnail_id($post->ID) );
+
+?>
 <div class="main-wrapper-area">
-
     <!-- ======= Start-Banner-Area ======= -->
-    <section class="banner-section about">
-        <div class="banner-content">
-            <h2>We are passionate about what we do, and it shows</h2>
-        </div>
-    </section>
-    <!-- ======= End-Banner-Area ======= -->
-
-    <div class="container page-content">
-        <section class="common-header mb-5">
-            <header class="page-header">
-                <h1 class="page-title">Our Staff</h1>
-            </header>
-        </section>
-        <div class="our-staff-block pt-5">
-            <div class="row">
-                <article class="staff-bio col-md-6">
-                    <header>
-                        <div class="headshot">
-                            <img src="https://lsbauctions.com/wp-content/uploads/2021/11/Untitled-design-13.jpg" alt="">
-                        </div>
-                        <h2 class="h2-as-h5">
-                            Spencer, Alyse &amp; Walker Hoglund <small>President</small>
-                        </h2>
-                    </header>
-                    <p>Spencer is our founder, he started Lock Stock &amp; Barrel in his college apartment in 2007 and has been selling online since 2002. After Grad School at Pepperdine, Spencer took Lock Stock &amp; Barrel full time and has guided it through tremendous growth, centered on delivering the best service quality possible. You’ll find Spencer’s labrador retrievers at work with him on a regular basis and when not at work, he’s probably driving the country with his family picking up your guns.</p>
-                </article>
-                <article class="staff-bio col-md-6">
-                    <header>
-                        <div class="headshot">
-                            <img src="https://lsbauctions.com/wp-content/uploads/2022/08/Untitled-design-8.jpg" alt="">
-                        </div>
-                        <h2 class="h2-as-h5">
-                            Chris Corcino <small>Acting COO</small>
-                        </h2>
-                    </header>
-                    <p>Chris has been working part time at LSB for the past several years while attending college. He recently graduated from Cal State Fullerton with a degree in Criminology. He's also a 5 Time United States Practical Shooting Association GrandMaster and World Champion Speed Shooter. Now after earning his degree, he is back as our acting COO, helping Spencer with the operations side of LSB.</p>
-                </article>
-                <article class="staff-bio col-md-6">
-                    <header>
-                        <div class="headshot">
-                            <img src="https://lsbauctions.com/wp-content/uploads/2022/07/Untitled-design-7-1.jpg" alt="">
-                        </div>
-                        <h2 class="h2-as-h5">
-                            Nanci Braunschweiger <small>Human Resources Manager</small>
-                        </h2>
-                    </header>
-                    <p>Nanci comes to LSB through her lovely daughter Alyse who is married to the CEO, Spencer. She had retired after thirty plus years in Human Resources when Spencer saw rapid growth occurring and he knew he needed an on-site experienced HR professional to help with HR so he offered Nanci a part time position. The rest of her time is spent with Alyse and Spencer’s son, Walker and her other grandchildren. When she’s not being “Noni” she can be found on the Pickleball court.</p>
-                </article>
-                <article class="staff-bio col-md-6">
-                    <header>
-                        <div class="headshot">
-                            <img src="https://lsbauctions.com/wp-content/uploads/2021/11/hs_lydia-rella.jpg" alt="Lydia Rella">
-                        </div>
-                        <h2 class="h2-as-h5">
-                            Lydia Rella <small>Office Manager</small>
-                        </h2>
-                    </header>
-                    <p>Lydia gets it done. And by “it”, we mean everything. Literally. From intake to shipping, from employee birthdays to company outings, and from opening to closing, Lydia keeps things moving. Boss-Lyd, Office Mom, Halloween Queen, whatever you want to call her, the shoe fits. Lydia is the whip crack we hear and the shoulder we cry on, aptly named Office Mom, we love and need her. </p>
-                </article>
-                <article class="staff-bio col-md-6">
-                    <header>
-                        <div class="headshot">
-                            <img src="https://lsbauctions.com/wp-content/uploads/2021/11/hs_amanda-rella.jpg" alt="Amanda Rella">
-                        </div>
-                        <h2 class="h2-as-h5">
-                            Amanda Rella <small>Consignor Liaison</small>
-                        </h2>
-                    </header>
-                    <p>Amanda is whatever we need, whenever we need it. She’s moved quickly up the ranks from literally counting bullets to our rockstar DConsignor Liaison. Nothing is beneath her and nothing is above her, well actually at 5’2?, almost everything is above her. When you have a question or need something done, she'll get it done.</p>
-                </article>
-                <article class="staff-bio col-md-6">
-                    <header>
-                        <div class="headshot">
-                            <img src="https://lsbauctions.com/wp-content/uploads/2021/11/hs_ryan-avena.jpg" alt="Ryan Avena">
-                        </div>
-                        <h2 class="h2-as-h5">
-                            Ryan Avena <small>Acquisitions Manager</small>
-                        </h2>
-                    </header>
-                    <p>Former Marine turned LSB Shipping Manager, turned LSB Acquisitions Manager, Ryan has climbed the ranks. If you have a firearm and it is within a day’s drive of LSB, Ryan is coming for it! Prompt and presentable, energetic and eager, Ryan is one of the engines that drives the acquisition at the company. Don’t be surprised if your phone rings and it’s Ryan calling to see if you need us to sell anything for you!</p>
-                </article>
-                <article class="staff-bio col-md-6">
-                    <header>
-                        <div class="headshot">
-                            <img src="https://lsbauctions.com/wp-content/uploads/2022/07/Untitled-design-6.jpg" alt="">
-                        </div>
-                        <h2 class="h2-as-h5">
-                            Austin Melling <small>Photography Manager</small>
-                        </h2>
-                    </header>
-                    <p>Austin is quickly turning into one of our best employee-referred hires. He comes to us with auction experience on a smaller scale but he's learning the ropes quickly. When he's not at work you can find him outside hiking or indoors hunched over a board game. Almost every department has tried to steal him so far, but shipping is holding on to this valuable team player.</p>
-                </article>
-                <article class="staff-bio col-md-6">
-                    <header>
-                        <div class="headshot">
-                            <img src="https://lsbauctions.com/wp-content/uploads/2021/11/hs_hector-rivera.jpg" alt="Hector Rivera">
-                        </div>
-                        <h2 class="h2-as-h5">
-                            Hector Rivera <small>Shipping Manager</small>
-                        </h2>
-                    </header>
-                    <p>Brand new to LSB, Hector is no stranger to the gun world. Customer turned employee, he’s a gun lover, father and frequent hunter. He’s been a great addition to the LSB staff, ensuring that our customers receive their purchases well-packaged and on-time.</p>
-                </article>
+<?php if($feat_image !=''){ ?>
+        <section class="banner-section about" style="background-image: url('<?php echo $feat_image; ?>');">
+  <?php if(get_the_content() !=''){ ?>
+            <div class="banner-content">
+                    <?php the_content(); ?>
             </div>
-        </div>
-    </div>
+  <?php } ?>
+        </section>
+<?php }
 
+$our_staff_head_title = get_field('_our_staff_head_title');
+$our_staff_repeater = get_field('_our_staff_repeater');
+
+?>
+    <!-- ======= End-Banner-Area ======= -->
+    <div class="container page-content">
+  <?php if($our_staff_head_title !=''){ ?>
+            <section class="common-header mb-5">
+                <header class="page-header">
+                    <h1 class="page-title"><?php echo $our_staff_head_title; ?></h1>
+                </header>
+            </section>
+    <?php } if($our_staff_repeater !=''){ ?>
+                <div class="our-staff-block pt-5">
+                    <div class="row">
+            <?php foreach($our_staff_repeater as $our_staff_repeater_val){ ?>
+                    <article class="staff-bio col-md-6">
+                        <header>
+                        <?php if($our_staff_repeater_val['_staff_member_image'] !=''){ ?>
+                        <div class="headshot">
+                                <img src="<?php echo $our_staff_repeater_val['_staff_member_image']['url']; ?>" alt="<?php echo $our_staff_repeater_val['_staff_member_image']['alt']; ?>">
+                            </div>
+                  <?php } if($our_staff_repeater_val['_staff__name'] !=''){ ?>
+                            <h2 class="h2-as-h5">
+                                <?php echo $our_staff_repeater_val['_staff__name']; ?><small><?php echo $our_staff_repeater_val['_designation']; ?></small>
+                          </h2>
+                  <?php } ?>
+                        </header>
+              <?php if($our_staff_repeater_val['_staff_description'] !=''){ ?>
+                        <?php echo $our_staff_repeater_val['_staff_description']; ?>
+             <?php }  ?>          
+                    </article>
+          <?php } ?>
+                    </div>
+                </div>
+      <?php } ?>
+    </div>
 </div>
 <!-- ======= End-Main-Area ======= -->
 <?php get_footer(); ?>
