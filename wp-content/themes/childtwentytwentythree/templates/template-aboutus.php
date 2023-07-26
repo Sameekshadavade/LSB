5dfa0bfa0f971c489015d9f3bbd36cf0db1315f5
AsheeshChapter247 <aaseesh.g@chapter247.com>
2023-07-26 12:27:05 +0530

    create and design acquisition page

--- wp-content/themes/childtwentytwentythree/templates/template-aboutus.php
+++ wp-content/themes/childtwentytwentythree/templates/template-aboutus.php
@@ -2,116 +2,124 @@
 
 <?php get_header(); ?>
 <!-- ======= Start-Main-Area ======= -->
-<?php  
+<?php
 
 $about_banner_desktop_image = get_field('_about_banner_desktop_image_');
 $banner_site_logo = get_field('_banner_site_logo');
 $banner_heading_text = get_field('_banner_heading_text');
 
 ?>
+<!-- ======= Start-Main-Area ======= -->
 <div class="main-wrapper-area">
 
     <!-- ======= Start-Banner-Area ======= -->
-<?php if($about_banner_desktop_image !='' || $banner_site_logo !='' || $banner_heading_text !=''){ ?>
-    <section class="banner-section about" style="background-image:url(<?php echo $about_banner_desktop_image['url']; ?>);">
-        <div class="banner-content">
-    <?php if($banner_site_logo !=''){ ?>    
-            <img class="lsb-brand" src="<?php echo $banner_site_logo['url']; ?>" alt="<?php echo $banner_site_logo['alt']; ?>">
-    <?php } if($banner_heading_text !=''){ ?>
-            <h2><?php echo $banner_heading_text; ?></h2>
-    <?php } ?>
-        </div>
-    </section>
-<?php } 
+    <?php if ($about_banner_desktop_image != '' || $banner_site_logo != '' || $banner_heading_text != '') { ?>
+        <section class="banner-section about" style="background-image:url(<?php echo $about_banner_desktop_image['url']; ?>);">
+            <div class="banner-content">
+                <?php if ($banner_site_logo != '') { ?>
+                    <img class="lsb-brand" src="<?php echo $banner_site_logo['url']; ?>" alt="<?php echo $banner_site_logo['alt']; ?>">
+                <?php }
+                if ($banner_heading_text != '') { ?>
+                    <h2><?php echo $banner_heading_text; ?></h2>
+                <?php } ?>
+            </div>
+        </section>
+    <?php }
 
 
-$about_us_title = get_field('_about_us_title');
-$left_side_title = get_field('_left_side_title');
-$left_side_content = get_field('_left_side_content');
-$right_side_history_image = get_field('_right_side_history_image');
+    $about_us_title = get_field('_about_us_title');
+    $left_side_title = get_field('_left_side_title');
+    $left_side_content = get_field('_left_side_content');
+    $right_side_history_image = get_field('_right_side_history_image');
 
-$lock_stock_head_title = get_field('_lock_stock_head_title');
-$difference_repeater = get_field('_difference_repeater');
+    $lock_stock_head_title = get_field('_lock_stock_head_title');
+    $difference_repeater = get_field('_difference_repeater');
 
-?>
+    ?>
     <!-- ======= End-Banner-Area ======= -->
 
     <div class="container page-content">
-        <?php if($about_us_title !=''){ ?>
+        <?php if ($about_us_title != '') { ?>
             <section class="common-header mb-5">
                 <header class="page-header">
                     <h1 class="page-title"><?php echo $about_us_title; ?></h1>
                 </header>
             </section>
-  <?php } if($left_side_title !='' || $left_side_content !='' || $right_side_history_image !=''){ ?>
-        <section class="history-section pt-md-5">
-            <div class="row">
-                <div class="col-md-8">
-                <?php if($left_side_title !=''){ ?>
-                        <h2><?php echo $left_side_title; ?></h2>
-                <?php } if($left_side_content !=''){ ?>
-                        <?php echo $left_side_content; ?>
-                <?php }  ?>
-                </div>
-      <?php if($right_side_history_image !=''){ ?>
-                <div class="col-md-4">
-                    <div class="banner">
-                        <img src="<?php echo $right_side_history_image['url']; ?>" alt="<?php echo $right_side_history_image['alt']; ?>">
+        <?php }
+        if ($left_side_title != '' || $left_side_content != '' || $right_side_history_image != '') { ?>
+            <section class="history-section pt-md-5">
+                <div class="row">
+                    <div class="col-md-8">
+                        <?php if ($left_side_title != '') { ?>
+                            <h2><?php echo $left_side_title; ?></h2>
+                        <?php }
+                        if ($left_side_content != '') { ?>
+                            <?php echo $left_side_content; ?>
+                        <?php }  ?>
                     </div>
+                    <?php if ($right_side_history_image != '') { ?>
+                        <div class="col-md-4">
+                            <div class="banner">
+                                <img src="<?php echo $right_side_history_image['url']; ?>" alt="<?php echo $right_side_history_image['alt']; ?>">
+                            </div>
+                        </div>
+                    <?php } ?>
                 </div>
-      <?php } ?>
-            </div>
-        </section>
-    <?php } if($lock_stock_head_title !='' || $difference_repeater !=''){ ?>
-        <section class="row lsb-difference mt-5 mb-5">
-            <div class="col">
-                <h2 class="section-heading"><span>The Lock, Stock &amp; Barrel Difference</span></h2>
-                <ul class="two-col circle-checkmark">
-            <?php foreach($difference_repeater as $difference_repeater_val){ ?>
-                <li>
-              <?php if($difference_repeater_val['_difference_title'] !=''){ ?>
-                        <h3><?php echo $difference_repeater_val['_difference_title']; ?></h3>
-              <?php } if($difference_repeater_val['_difference_content'] !=''){ ?>    
-                        <?php echo $difference_repeater_val['_difference_content']; ?>
-                <?php } ?>
-                    </li>
-                <?php } ?>
-                </ul>
-            </div>
-        </section>
-<?php 
-}
-$buyer_feedback_head_title = get_field('_buyer_feedback_head_title');
-$buyer_feedback_content = get_field('_buyer_feedback_content');
-$buyer_feedback_notice = get_field('_buyer_feedback_notice');
-$feedback_repeater = get_field('_feedback_repeater');
+            </section>
+        <?php }
+        if ($lock_stock_head_title != '' || $difference_repeater != '') { ?>
+            <section class="row lsb-difference mt-5 mb-5">
+                <div class="col">
+                    <h2 class="section-heading"><span>The Lock, Stock &amp; Barrel Difference</span></h2>
+                    <ul class="two-col circle-checkmark">
+                        <?php foreach ($difference_repeater as $difference_repeater_val) { ?>
+                            <li>
+                                <?php if ($difference_repeater_val['_difference_title'] != '') { ?>
+                                    <h3><?php echo $difference_repeater_val['_difference_title']; ?></h3>
+                                <?php }
+                                if ($difference_repeater_val['_difference_content'] != '') { ?>
+                                    <?php echo $difference_repeater_val['_difference_content']; ?>
+                                <?php } ?>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </div>
+            </section>
+        <?php
+        }
+        $buyer_feedback_head_title = get_field('_buyer_feedback_head_title');
+        $buyer_feedback_content = get_field('_buyer_feedback_content');
+        $buyer_feedback_notice = get_field('_buyer_feedback_notice');
+        $feedback_repeater = get_field('_feedback_repeater');
 
-?>
+        ?>
         <section class="row user-feedback">
             <div class="col">
-            <?php if($buyer_feedback_head_title !=''){ ?>
+                <?php if ($buyer_feedback_head_title != '') { ?>
                     <h2 class="section-heading"><span><?php echo $buyer_feedback_head_title; ?></span></h2>
-            <?php } if($buyer_feedback_content !=''){ ?>
-                <?php echo $buyer_feedback_content; ?>
-            <?php } if($buyer_feedback_notice !=''){ ?>
+                <?php }
+                if ($buyer_feedback_content != '') { ?>
+                    <?php echo $buyer_feedback_content; ?>
+                <?php }
+                if ($buyer_feedback_notice != '') { ?>
                     <p class="note-block">
                         <?php echo $buyer_feedback_notice; ?>
                     </p>
-        <?php } ?>
+                <?php } ?>
                 <div class="row">
-        <?php if($feedback_repeater !=''){
-                foreach($feedback_repeater as $feedback_repeater_val){ ?>
-                    <div class="col-sm-6 col-md-3 feedback-link">
-          <?php if($feedback_repeater_val['_feedback_link'] !='' && $feedback_repeater_val['_feedback_image'] !=''){ ?>
-                    <a href="<?php echo $feedback_repeater_val['_feedback_link']['url']; ?>" target="_blank">
-                            <img src="<?php echo $feedback_repeater_val['_feedback_image']['url']; ?>" alt="<?php echo $feedback_repeater_val['_feedback_image']['alt']; ?>">
-                            <span><?php echo $feedback_repeater_val['_feedback_link']['title']; ?></span>
-                            <span class="screen-reader-text">Opens a new window</span>
-                        </a>
-              <?php } ?>
-                    </div>
-          <?php } 
-            } ?>
+                    <?php if ($feedback_repeater != '') {
+                        foreach ($feedback_repeater as $feedback_repeater_val) { ?>
+                            <div class="col-sm-6 col-md-3 feedback-link">
+                                <?php if ($feedback_repeater_val['_feedback_link'] != '' && $feedback_repeater_val['_feedback_image'] != '') { ?>
+                                    <a href="<?php echo $feedback_repeater_val['_feedback_link']['url']; ?>" target="_blank">
+                                        <img src="<?php echo $feedback_repeater_val['_feedback_image']['url']; ?>" alt="<?php echo $feedback_repeater_val['_feedback_image']['alt']; ?>">
+                                        <span><?php echo $feedback_repeater_val['_feedback_link']['title']; ?></span>
+                                        <span class="screen-reader-text">Opens a new window</span>
+                                    </a>
+                                <?php } ?>
+                            </div>
+                    <?php }
+                    } ?>
                 </div>
             </div>
         </section>
--- wp-content/themes/childtwentytwentythree/templates/template-acquisition.php
+++ wp-content/themes/childtwentytwentythree/templates/template-acquisition.php
@@ -1,16 +1,84 @@
-<?php /* Template Name: Acquisition Page */ ?>
+<?php /* Template Name: Acquisition */ ?>
 
 <?php get_header(); ?>
 
+<!-- ======= Start-Main-Area ======= -->
+<div class="main-wrapper-area">
 
+    <!-- ======= Start-Banner-Area ======= -->
+    <section class="banner-section spencer">
+        <div class="banner-content">
+            <a class="play-btn" href="https://www.youtube.com/watch?v=X3_kKr9RdQ0" alt="2013 SASS Hall Of Fame Induction Video" data-rel="lightbox-gallery-dbAp3AM1" data-magnific_type="video" data-rl_title="" data-rl_caption="" title="">
+                <img src="http://localhost/Lsb/wp-content/uploads/2023/07/icon-youtube-white.svg" alt="Play Video">
+            </a>
+            <p>Click Here To Play</p>
+            <h2>2013 SASS Hall Of Fame Induction Video</h2>
+        </div>
+    </section>
+    <!-- ======= End-Banner-Area ======= -->
 
+    <div class="container page-content">
+        <section class="common-header mb-5">
+            <header class="page-header">
+                <h1 class="page-title">Spencer "Lead Dispencer" Hoglund</h1>
+            </header>
+        </section>
+        <div class="our-staff-block pt-5">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                    <div class="spencer-img-block">
+                        <img class="headshot" src="http://localhost/Lsb/wp-content/uploads/2023/07/single-user-img.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-8 col-md-4 col-sm-12 col-xs-12">
+                    <div class="spencer-content-block">
+                        <p>
+                            Spencer Hoglund was born and raised in the San Fernando Valley, California. Spencer has focused much of his
+                            life
+                            on his dual passions of competitive shooting and gun collecting. He has traveled the World, competing and
+                            making
+                            friends across 18 countries, and over 40 states. But Spencer’s true notoriety came under a different name...
+                        </p>
+                        <p>
+                            Lead Dispencer; It’s one of the best aliases in Cowboy Action Shooting, and certainly one befitting a World
+                            Champion. In the smoky, fast-paced, high adrenaline world of Cowboy Action Shooting, no name draws greater
+                            respect from fans, or fear, from opponents, than Spencer’s.
+                        </p>
+                        <p>
+                            After competing in Sporting Clays, Spencer Hoglund, aka Lead Dispencer, became a three-time Overall SASS World
+                            Champion (6 Time Category Winner), a Five Time Overall SASS National Champion, and his State, Regional, and
+                            local wins too numerous to be listed here. Many familiar with the evolution of the sport will tell you that
+                            Spencer changed the game. Spencer was for the 2000’s what China Camp was for the 1990’s, and when you see no
+                            one
+                            over 30 in the top 20 at “End of Trail” – you can thank Spencer for that.
+                        </p>
+                        <p>
+                            Top Shot, Sons Of Guns, American guns, Triggers, American Shooter, Shooting USA, Cowboys, Catch 22, The Wild,
+                            Wild, West... are some of his television appearances alone, showcasing his incomparable skills with a rifle,
+                            pistol, and shotgun.
+                        </p>
+                        <p>
+                            In addition to his shooting career, Spencer was quite an athlete, having been the Team Captain of the
+                            California
+                            State University Northridge Water Ski Team, where he earned his Undergraduate degree, graduating Summa Cum
+                            Laude.
+                        </p>
+                        <p>
+                            Spencer continued his education by earning his Master’s in Business Administration, at Pepperdine University’s
+                            Graziadio School of Business – where he graduated second in his class.
+                        </p>
+                        <p>
+                            Now, he heads up the crew at Lock Stock and Barrel Investments. The idea for an internet firearms and
+                            accessories retailer, combined with an ebay outlet was his, and has been both rewarding and challenging.
+                            Spencer’s nature is to always be prepared for that next challenge.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-
-
-
-
-
-
-
+</div>
+<!-- ======= End-Main-Area ======= -->
 
 <?php get_footer(); ?>
