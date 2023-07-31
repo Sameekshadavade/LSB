d43a5d985cd782c9c03774beddcbec1711bb25cd
AsheeshChapter247 <aaseesh.g@chapter247.com>
2023-07-31 18:50:59 +0530

    design on faq page

--- wp-content/themes/childtwentytwentythree/templates/template-faqs.php
+++ wp-content/themes/childtwentytwentythree/templates/template-faqs.php
@@ -10,21 +10,82 @@
                 <h1 class="page-title">FREQUENTLY ASKED QUESTIONS</h1>
             </header>
         </section>
-        <section class="faq-section pt-5">
+        <section class="faq-section pt-md-5 pt-sm-3">
             <div class="row">
                 <div class="col-lg-7">
-                    <p>We are here to help in anyway we can. Below are a list of the most frequently asked questions.
-                        Take a
-                        look and see if the answer to your question is there. If it isn't, then feel free to use the
-                        adjoining
-                        contact form to reach out to us. We will respond as quickly as possible.
-                    </p>
+                    <div class="faq-description-block">
+                        <p>We are here to help in anyway we can. Below are a list of the most frequently asked questions.
+                            Take a
+                            look and see if the answer to your question is there. If it isn't, then feel free to use the
+                            adjoining
+                            contact form to reach out to us. We will respond as quickly as possible.
+                        </p>
+                    </div>
+
+                    <div class="faq-list">
+                        <div class="accordion accordion-flush faq-inner-block" id="accordionFlushExample">
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="flush-headingOne">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                        Do you handle local transfers?
+                                    </button>
+                                </h2>
+                                <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                                    <div class="accordion-body">
+                                        <span class="faq-title"><strong>ANSWER:</strong></span>
+                                        <p>If you purchase a gun from another online seller, dealer, or auction, we can handle the transfer of the firearm. We CANNOT accept any firearm that is considered an Assault Weapon in California, if you need to ask – the answer is likely no. We also DO NOT accept transfers on AR or AK type rifles. We are not that kind of shop. Additionally, transfers are reserved for customers familiar with the California Roster of Handguns Certified for Sale and the relevant Exemptions. Make sure you can actually register the gun you bought.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="flush-headingTwo">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                                        What forms of payment do you accept?
+                                    </button>
+                                </h2>
+                                <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                                    <div class="accordion-body">
+                                        <span class="faq-title"><strong>ANSWER:</strong></span>
+                                        <p>If you purchase a gun from another online seller, dealer, or auction, we can handle the transfer of the firearm. We CANNOT accept any firearm that is considered an Assault Weapon in California, if you need to ask – the answer is likely no. We also DO NOT accept transfers on AR or AK type rifles. We are not that kind of shop. Additionally, transfers are reserved for customers familiar with the California Roster of Handguns Certified for Sale and the relevant Exemptions. Make sure you can actually register the gun you bought.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="flush-headingThree">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                                        Do you offer a layaway on the firearms?
+                                    </button>
+                                </h2>
+                                <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                                    <div class="accordion-body">
+                                        <span class="faq-title"><strong>ANSWER:</strong></span>
+                                        <p>If you purchase a gun from another online seller, dealer, or auction, we can handle the transfer of the firearm. We CANNOT accept any firearm that is considered an Assault Weapon in California, if you need to ask – the answer is likely no. We also DO NOT accept transfers on AR or AK type rifles. We are not that kind of shop. Additionally, transfers are reserved for customers familiar with the California Roster of Handguns Certified for Sale and the relevant Exemptions. Make sure you can actually register the gun you bought.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="flush-headingFour">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+                                        What is a DROS?
+                                    </button>
+                                </h2>
+                                <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
+                                    <div class="accordion-body">
+                                        <span class="faq-title"><strong>ANSWER:</strong></span>
+                                        <p>If you purchase a gun from another online seller, dealer, or auction, we can handle the transfer of the firearm. We CANNOT accept any firearm that is considered an Assault Weapon in California, if you need to ask – the answer is likely no. We also DO NOT accept transfers on AR or AK type rifles. We are not that kind of shop. Additionally, transfers are reserved for customers familiar with the California Roster of Handguns Certified for Sale and the relevant Exemptions. Make sure you can actually register the gun you bought.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-lg-5 sidebar">
-                    <h2>
-                        Still have a question?
-                        <small>Send us a message and we will be happy to help.</small>
-                    </h2>
+                <div class="col-lg-5">
+                    <div class="faq-sidebar">
+                        <h2>
+                            Still have a question?
+                            <small>Send us a message and we will be happy to help.</small>
+                        </h2>
+                    </div>
                 </div>
             </div>
         </section>
