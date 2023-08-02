f23c0d0714bceb6c4da90272631f169a0f129159
dharmendra-c247 <dharmendra.v@chapter247.com>
2023-08-02 10:56:40 +0530

    worked on faq page

--- wp-content/themes/childtwentytwentythree/templates/template-faqs.php
+++ wp-content/themes/childtwentytwentythree/templates/template-faqs.php
@@ -5,82 +5,50 @@
 <!-- ======= Start-Main-Area ======= -->
 <div class="main-wrapper-area">
     <div class="container page-content">
-    <?php if(get_the_title() !=''){ ?>
+    <?php //if(get_the_title() !=''){ ?>
         <section class="common-header mb-5">
             <header class="page-header">
                 <h1 class="page-title"><?php the_title(); ?></h1>
             </header>
         </section>
+<?php  
+$faq_page_repeater = get_field('_faq_page_repeater_');
+$faq_form_shortcode = get_field('faq_form_shortcode');
+
+?>
         <section class="faq-section pt-md-5 pt-sm-3">
             <div class="row">
         <?php if(get_the_content() !=''){ ?>
                 <div class="col-lg-7">
                     <div class="faq-description-block">
-                        <p>We are here to help in anyway we can. Below are a list of the most frequently asked questions.
-                            Take a
-                            look and see if the answer to your question is there. If it isn't, then feel free to use the
-                            adjoining
-                            contact form to reach out to us. We will respond as quickly as possible.
-                        </p>
+                        <?php the_content(); ?>
                     </div>
-
+        <?php } if($faq_page_repeater !=''){ ?>
                     <div class="faq-list">
                         <div class="accordion accordion-flush faq-inner-block" id="accordionFlushExample">
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="flush-headingOne">
+                    <?php foreach($faq_page_repeater as $faq_page_repeater_val){ ?>     
+                        <div class="accordion-item">
+              <?php if($faq_page_repeater_val['_faq_question_'] !=''){ ?>
+                        <h2 class="accordion-header" id="flush-headingOne">
                                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                                        Do you handle local transfers?
+                                        <?php echo $faq_page_repeater_val['_faq_question_']; ?>
                                     </button>
                                 </h2>
+              <?php } if($faq_page_repeater_val['_faq_answer_'] !=''){ ?>
                                 <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
                                     <div class="accordion-body">
                                         <span class="faq-title"><strong>ANSWER:</strong></span>
-                                        <p>If you purchase a gun from another online seller, dealer, or auction, we can handle the transfer of the firearm. We CANNOT accept any firearm that is considered an Assault Weapon in California, if you need to ask – the answer is likely no. We also DO NOT accept transfers on AR or AK type rifles. We are not that kind of shop. Additionally, transfers are reserved for customers familiar with the California Roster of Handguns Certified for Sale and the relevant Exemptions. Make sure you can actually register the gun you bought.</p>
+                                        <?php echo $faq_page_repeater_val['_faq_answer_']; ?>
                                     </div>
                                 </div>
+              <?php } ?>
                             </div>
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="flush-headingTwo">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                                        What forms of payment do you accept?
-                                    </button>
-                                </h2>
-                                <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-                                    <div class="accordion-body">
-                                        <span class="faq-title"><strong>ANSWER:</strong></span>
-                                        <p>If you purchase a gun from another online seller, dealer, or auction, we can handle the transfer of the firearm. We CANNOT accept any firearm that is considered an Assault Weapon in California, if you need to ask – the answer is likely no. We also DO NOT accept transfers on AR or AK type rifles. We are not that kind of shop. Additionally, transfers are reserved for customers familiar with the California Roster of Handguns Certified for Sale and the relevant Exemptions. Make sure you can actually register the gun you bought.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="flush-headingThree">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                                        Do you offer a layaway on the firearms?
-                                    </button>
-                                </h2>
-                                <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-                                    <div class="accordion-body">
-                                        <span class="faq-title"><strong>ANSWER:</strong></span>
-                                        <p>If you purchase a gun from another online seller, dealer, or auction, we can handle the transfer of the firearm. We CANNOT accept any firearm that is considered an Assault Weapon in California, if you need to ask – the answer is likely no. We also DO NOT accept transfers on AR or AK type rifles. We are not that kind of shop. Additionally, transfers are reserved for customers familiar with the California Roster of Handguns Certified for Sale and the relevant Exemptions. Make sure you can actually register the gun you bought.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="accordion-item">
-                                <h2 class="accordion-header" id="flush-headingFour">
-                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
-                                        What is a DROS?
-                                    </button>
-                                </h2>
-                                <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
-                                    <div class="accordion-body">
-                                        <span class="faq-title"><strong>ANSWER:</strong></span>
-                                        <p>If you purchase a gun from another online seller, dealer, or auction, we can handle the transfer of the firearm. We CANNOT accept any firearm that is considered an Assault Weapon in California, if you need to ask – the answer is likely no. We also DO NOT accept transfers on AR or AK type rifles. We are not that kind of shop. Additionally, transfers are reserved for customers familiar with the California Roster of Handguns Certified for Sale and the relevant Exemptions. Make sure you can actually register the gun you bought.</p>
-                                    </div>
-                                </div>
+                        <?php } ?>
                             </div>
                         </div>
                     </div>
                 </div>
+      <?php } ?>
                 <div class="col-lg-5">
                     <div class="faq-sidebar">
                         <h2>
